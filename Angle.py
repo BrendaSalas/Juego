c64411cd3aac8fa5458fdbe9b4da81f4696461f2
BrendaSalas <l24200100@pachuca.tecnm.mx>
2024-09-27 20:55:20 -0600

    Delete Angle.py

--- Angle.py
+++
@@ -1,8 +0,0 @@
-def third_angle(angle1, angle2):
-    # Calcular el tercer ángulo
-    return 180 - (angle1 + angle2)
-
-# Ejemplo de uso
-angle1 = 60
-angle2 = 70
-print(third_angle(angle1, angle2))  # Devolverá 50
